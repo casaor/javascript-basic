2e4807c14f7e3e2a1db21b070b320023a2de16cc
carlota sas <casaor@outlook.com>
2020-12-02 12:47:49 +0100

    exercici 5

---
+++ Exercici 5/notes.js
@@ -0,0 +1,15 @@
+function resultat() {
+
+    let notaExamen = Number(document.getElementById("nota").value);
+
+    if (notaExamen >=0 && notaExamen < 5){
+        alert("Has suspès l'examen amb un " + notaExamen);
+
+      }else if (notaExamen >= 5 && notaExamen <= 10){
+        alert("Felicitats, has aprovat amb un " + notaExamen);
+
+      }else{
+        alert("No has introduït bé la nota");
+      }
+
+}
